f41bd55ca03573b84d7299f191b9226383ef6734
fezfez <demonchaux.stephane@gmail.com>
2014-06-02 18:03:57 +0200

    Add strict comparaison

--- src/Packagist/Api/Result/Factory.php
+++ src/Packagist/Api/Result/Factory.php
@@ -31,7 +31,7 @@ class Factory
     {
         $resultCollection = clone $resultCollection;
 
-        if (isset($results['results'])) {
+        if (isset($results['results']) === true && is_array($results['results'])) {
             foreach ($results['results'] as $key => $result) {
                 $resultCollection->append(new Result($result));
             }
@@ -48,25 +48,25 @@ class Factory
     {
         $package = $data['package'];
 
-        if (isset($package['maintainers']) && is_array($package['maintainers'])) {
+        if (isset($package['maintainers']) === true && is_array($package['maintainers'])) {
             foreach ($package['maintainers'] as $key => $maintainer) {
                 $package['maintainers'][$key] = new Maintainer($maintainer);
             }
         }
 
-        if (isset($package['downloads']) && is_array($package['downloads'])) {
+        if (isset($package['downloads']) === true && is_array($package['downloads'])) {
             $package['downloads'] = new Downloads($package['downloads']);
         }
 
-        if (isset($package['versions']) && is_array($package['versions'])) {
+        if (isset($package['versions']) === true && is_array($package['versions'])) {
             foreach ($package['versions'] as $branch => $version) {
-                if (isset($version['authors']) && is_array($version['authors'])) {
+                if (isset($version['authors']) === true && is_array($version['authors'])) {
                     foreach ($version['authors'] as $key => $author) {
                         $version['authors'][$key] = new Author($author);
                     }
                 }
                 $version['source'] = new Source($version['source']);
-                if (isset($version['dist']) && is_array($version['dist'])) {
+                if (isset($version['dist']) === true && is_array($version['dist'])) {
                     $version['dist'] = new Dist($version['dist']);
                 }
 
