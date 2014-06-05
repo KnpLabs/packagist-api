6d14f20e5b1856f7e7521eb166221b20f78270b0
fezfez <demonchaux.stephane@gmail.com>
2014-06-05 22:23:07 +0200

    Change Factory as propose in discution

--- src/Packagist/Api/ClientFactory.php
+++ src/Packagist/Api/ClientFactory.php
@@ -10,12 +10,20 @@ class ClientFactory
     /**
      * Create instance of PackagistApiClient
      *
-     * @param string $packagistUrl
+     * @param null|ClientInterface $httpClient
+     * @param string               $packagistUrl
      *
      * @return \Packagist\Api\Client
      */
-    public static function getInstance($packagistUrl = "https://packagist.org")
+    public static function getInstance(
+        ClientInterface $httpClient = null,
+        $packagistUrl = "https://packagist.org"
+    )
     {
+        if ($httpClient === null) {
+            $httpClient = new HttpClient();
+        }
+
         return new Client(
             new HttpClient(),
             new Factory(),
