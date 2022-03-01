c676ce011eabad4a4eaa3748162772b92b10f7a2
Robbie Averill <robbie@averill.co.nz>
2022-03-01 00:16:43 -0800

    Fix bug where packages with a null description would cause a return type error
    Also adds .editorconfig configuration for IDE compatibility

--- spec/Packagist/Api/Result/FactorySpec.php
+++ spec/Packagist/Api/Result/FactorySpec.php
@@ -82,8 +82,15 @@ class FactorySpec extends ObjectBehavior
     function it_creates_packages_with_null_source()
     {
         $data = json_decode(file_get_contents('spec/Packagist/Api/Fixture/get_null_source.json'), true);
-
         $this->create($data)->shouldHaveType('Packagist\Api\Result\Package');
+    }
+
+    function it_creates_packages_with_null_description()
+    {
+        $data = json_decode(file_get_contents('spec/Packagist/Api/Fixture/get_null_description.json'), true);
+        $result = $this->create($data);
+        $result->shouldHaveType('Packagist\Api\Result\Package');
+        $result->getDescription()->shouldBe('');
     }
 
     public function getMatchers(): array
